b4d743e8710e2aae7cdff04beaaea21999f96c91
adam wood <a_wood60810@fanshaweonline.ca>
2019-04-11 02:02:19 -0400

    more js content

--- js/about.js
+++ js/about.js
@@ -2,7 +2,12 @@
     console.log("about ready");
     
     var aboutBut = document.querySelectorAll(".aboutBut"),
-        aboutP = document.querySelector("#aboutUs p");
+        aboutP = document.querySelector("#aboutUs p"),
+        statImg = document.querySelector(".statImg"),
+        back = document.querySelector(".statBack"),
+        next = document.querySelector(".statNext");
+    
+    var counter = 0;
     
     const aboutAr = [
         ["Protect, enhance and regenerate a viable, multi-species, year-round fishery."],
@@ -13,11 +18,36 @@
         ["Increase public awareness of the unique diversity of the Thames River Watershed, its challenges, its recreational opportunities and its vital importance to our community."]
     ];
     
-   aboutBut.forEach(but => {
-       but.addEventListener("click", function(e) {
-           console.log("aboutbut click");
-           aboutP.textContent = aboutAr[this.dataset.aboutref][0];
-       });
-   }); 
+    function animateStat() {
+        const offSet = 375;
+        
+        if (counter > 3) {
+            counter = 0;
+        }
+        if (counter < 0) {
+            counter = 3;
+        }
+        
+        totalOffSet = counter * offSet + "px";
+        
+        statImg.style.bottom = totalOffSet;
+    }
+    
+    back.addEventListener("click", function(e) {
+        counter -= 1;
+        animateStat();
+    });
+    
+    next.addEventListener("click", function(e) {
+        counter += 1;
+        animateStat();
+    });
+    
+    aboutBut.forEach(but => {
+        but.addEventListener("click", function(e) {
+            console.log("aboutbut click");
+            aboutP.textContent = aboutAr[this.dataset.aboutref][0];
+        });
+    }); 
     
 })();
