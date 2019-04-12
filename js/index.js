a5e2f5db42f52f6d0ea4d29cf72a3f6a5a4254f4
adam wood <a_wood60810@fanshaweonline.ca>
2019-04-12 00:02:11 -0400

    js transitions

--- js/index.js
+++ js/index.js
@@ -3,9 +3,16 @@
     
     var next = document.querySelector('.heroNext'),
         back = document.querySelector('.heroBack'),
-        image = document.querySelector('#imgTwo');
+        image = document.querySelector('#imgTwo'),
+        overlayCon = document.querySelectorAll('.homeOverlayCon'),
+        overlays = document.querySelectorAll('.homeOverlay'),
+        actImg = document.querySelector(".bui img");
     
     var counter = 1;
+    
+    let imgHeight = actImg.height;
+    var imgPer = imgHeight / 158 * 100,
+        padPer = 50 / 158 * 100;
     
     function swapImg() {
         if (counter > 3) {
@@ -15,6 +22,26 @@
             counter = 3;
         }
         image.src = `images/hero${counter}.jpg`;
+    }
+    
+    function overlayHome(nameSrc) {
+        overlays.forEach(overlay => {
+            if (nameSrc === overlay.dataset.homeoverlay) {
+                overlay.style.opacity = "1";
+                //overlay.style.bottom = "0px";
+                overlay.style.height = imgPer - padPer + "%";
+            }
+        });
+    }
+    
+    function overlayHomeDone(nameSrc) {
+        overlays.forEach(overlay => {
+            if (nameSrc === overlay.dataset.homeoverlay) {
+                overlay.style.opacity = "0";
+                //overlay.style.bottom = "-135px";
+                //overlay.style.height = "0px";
+            }
+        });
     }
     
     next.addEventListener("click", function(e) {
@@ -27,6 +54,25 @@
         swapImg();
     });
     
+    overlayCon.forEach(con => {
+        con.addEventListener("mouseover", function(e) {
+            let nameSrc = this.className.split(" ")[1];
+            overlayHome(nameSrc);
+            con.addEventListener("mouseleave", function(e) {
+                overlayHomeDone(nameSrc);
+            });
+        });
+    });
+    
+    
+    
+    
+    //overlayCon.addEventListener("mouseover", function(e) {
+        //overlayHome();
+        //overlayCon.addEventListener("mouseleave", function(e) {
+            //overlay.style.bottom = "-110px";
+        //});
+    //});
     
     
 })();
